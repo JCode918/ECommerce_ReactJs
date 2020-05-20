fd6f8fc272e18b79b11270c71a575a8e4619a450
JCode918 <Willard.Joseph26@gmail.com>
2020-05-19 23:00:37 -0400

    Update our menu-items to navigate withRouther Higer Order Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import Homepage from "./pages/homepage/homepage.component";
-import './homepage.styles.scss'
+import "./homepage.styles.scss";
 import "./App.css";
+
+const HatsPage = () => (
+  <div>
+    <h1> Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact={true} path="/" component={Homepage} />
+        <Route exact={true} path="/shop/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
