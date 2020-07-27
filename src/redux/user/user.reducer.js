ac1349e15d4902f812f7a4ce2c06a8d6bccf6f99
Willard Joseph Linux Laptop <Willard.Joseph26@gmail.com>
2020-07-27 16:54:12 -0400

    Redux Added To Project

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -6,7 +6,7 @@ const userReducer = (state = INITIAL_STATE, action) => {
     switch(action.type){
         case 'SET_CURRENT_USER': return {
             ...state,
-            currentuser: action.payload
+            currentUser: action.payload
         }
 
         default:
